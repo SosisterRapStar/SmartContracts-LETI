6718386a4bd2b2233c805dc2cb6a076a64a704e8
SosisterRapStar <timoshenkovvanya@gmail.com>
2025-12-24 18:15:52 +0300

    fix

--- Task_04.sol
+++ Task_04.sol
@@ -14,17 +14,3 @@ contract Task_04 {
        return balances[_addr];
    }
 }
-
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-contract Task_04 {
-   mapping(address => uint256) public balances; // Маппинг адресов на балансы
-
-   function setBalance(address _addr, uint256 _balance) external {
-       balances[_addr] = _balance; // Установка баланса для указанного адреса
-   }
-
-   // Функция для получения баланса по адресу
-
-}
